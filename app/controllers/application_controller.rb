5658a2ee88c82234ce6c30d461dcbe2006e1591e
Larry LeSure <lesure1@gmail.com>
2015-03-03 22:50:30 +0000

    Added username to users.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
   end
+			
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:sign_up) << :username
+		devise_parameter_sanitizer.for(:account_update) << :username
+  end
 
 end
